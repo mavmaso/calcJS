4c8a2d63c03ec30d70eafbea79cfcda8341006be
Marcos Soares <mavmaso@gmail.com>
2019-03-10 01:17:56 -0300

    aula c05

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -1,23 +1,36 @@
 class CalcController {
 
   constructor(){
-    this._displayCalc = "0";
-    this._dataAtual;
+    this._displayCalcEl = '0';
+    this._currentDate;
+    this.initialize();
+  }
+
+  initialize() {
+
+    let displayCalcEl = document.querySelector('#display');
+    let dataEl = document.querySelector('#data');
+    let timeEL = document.querySelector('#hora');
+    
+    displayCalcEl.innerHTML = "4567";
+    dataEl.innerHTML = "01/01/2020";
+    timeEL.innerHTML = "00:00";
   }
 
   get displayCalc(){
-    return this._displayCalc;
+    return this._displayCalcEl.innerHTML;
   }
 
   set displayCalc(valor){
     this._displayCalc = valor;
   }
 
-  get dataAtual(){
-    return this._dataAtual;
+  get currentDate(){
+    return this._currentDate;
   }
 
-  set dataAtual(valor){
-    this._dataAtual = valor;
+  set currentDate(valor){
+    this._currentDate = valor;
   }
+  
 }
